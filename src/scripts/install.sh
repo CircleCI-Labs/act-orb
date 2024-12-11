88c6d55a9baa6f285c0bee48a37724fc507a6bc8
james-crowley <jim.crowley@circleci.com>
2024-12-11 12:43:39 -0500

    fix: Reworked install script to avoid path issues

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -4,8 +4,8 @@
 BIN_DIR_FLAG=""
 DEBUG_FLAG=""
 
-# Check if a custom bin directory is set
-if [[ "${ORB_VAL_BIN_DIR}" != "./bin" ]]; then
+# Add bin-dir flag if a custom directory is provided
+if [[ -n "${ORB_VAL_BIN_DIR}" && "${ORB_VAL_BIN_DIR}" != "./bin" ]]; then
     BIN_DIR_FLAG="-b ${ORB_VAL_BIN_DIR}"
 fi
 
@@ -14,5 +14,14 @@ if [[ "${ORB_VAL_DEBUG}" == "true" ]]; then
     DEBUG_FLAG="-d"
 fi
 
-# Install Act with the specified version, bin-dir, and debug options
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash -s -- "${BIN_DIR_FLAG}" "${DEBUG_FLAG}" "${ORB_VAL_VERSION}"
+# Install Act with the correct options
+INSTALL_CMD="curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash -s --"
+
+# Construct the full command with all flags
+FULL_CMD="$INSTALL_CMD ${BIN_DIR_FLAG} ${DEBUG_FLAG} ${ORB_VAL_VERSION}"
+
+# Echo command for debugging
+echo "Running: $FULL_CMD"
+
+# Execute the command
+eval "$FULL_CMD"
